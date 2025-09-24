01f86c712400341a0b145f6d8d0d5e75898b2ff0
srivats <srivats666@gmail.com>
2025-09-24 11:13:17 -0700

    Codemate intial pnpm workspace setup with antfu linting.

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import simpleSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+
+export default [
+  { ...antfu },
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx,vue}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: [
+          './tsconfig.base.json',
+          './client/tsconfig.json',
+          './server/tsconfig.json',
+          './shared/tsconfig.json'
+        ],
+        tsconfigRootDir: process.cwd()
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'simple-import-sort': simpleSort,
+      'unused-imports': unusedImports
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: [
+            './tsconfig.base.json',
+            './client/tsconfig.json',
+            './server/tsconfig.json',
+            './shared/tsconfig.json'
+          ]
+        }
+      }
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+      ],
+      'prefer-const': 'warn',
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'variableLike',
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow'
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase']
+        }
+      ],
+      'import/order': 'off'
+    }
+  }
+]
